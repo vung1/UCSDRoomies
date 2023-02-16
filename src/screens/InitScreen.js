f2dfac6b7d94bf79450bf6f40173830075c6494f
Danlin <jiangdl0129@gmail.com>
2023-02-15 21:30:06 -0800

    pre-sprint1

--- src/screens/InitScreen.js
+++ src/screens/InitScreen.js
@@ -4,7 +4,7 @@ import {View, Text, StyleSheet} from 'react-native';
 const InitScreen = () => {
   return (
     <View style={styles.pageContainer}>
-      <Text>hi</Text>
+      <Text>Sprint1</Text>
     </View>
   );
 };
