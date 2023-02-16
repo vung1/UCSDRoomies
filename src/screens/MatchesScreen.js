0eaa9e6696785e3389495a22d42a20101086b21e
Danlin <jiangdl0129@gmail.com>
2023-02-16 14:46:35 -0800

    added messaging

--- src/screens/MatchesScreen.js
+++ src/screens/MatchesScreen.js
@@ -6,7 +6,7 @@ import users from '../../assets/data/users';
 const MatchesScreen = () => {
   return (
     <SafeAreaView style={styles.ver_container}>
-      {/* <View style={styles.container}>
+      <View style={styles.container}>
         <Text style={{fontWeight: 'bold', fontSize: 24, color: '#247DCF'}}>
           New Matches
         </Text>
@@ -21,8 +21,8 @@ const MatchesScreen = () => {
             ))}
           </View>
         </ScrollView>
-      </View> */}
-      <View>
+      </View>
+      <View style={styles.message_area}>
       <ScrollView style={styles.scrollView} vertical={true}>
               <View style={styles.container}>
                 {users.map(user => (
@@ -31,7 +31,17 @@ const MatchesScreen = () => {
                   <View style={styles.user} key={user.id}>
                     <Image source={{uri: user.image}} style={styles.simp_image} />
                   </View>
-                  <View><Text style={styles.message}>{user.messages[0]}</Text></View>
+                  <View style={styles.message_mid}>
+                    <Text style={styles.msg_name}>{user.name}</Text>
+                    <Text style={styles.message}>{user.messages[0].split("\n")[0]}</Text>
+                  </View>
+                  <View >
+                    <Text style={styles.time}>
+                    </Text>
+                    <Text style={styles.time}>{user.messages[0].split("\n").pop()}
+                    </Text>
+                  </View>
+                  
                 </View> : null
                 
               ))}
@@ -55,6 +65,7 @@ const styles = StyleSheet.create({
   },
   container: {
     padding: 10,
+    flex: 1,
   },
   users: {
     flexDirection: 'row',
@@ -90,15 +101,43 @@ const styles = StyleSheet.create({
     textAlign: "center",
     lineHeight: 40,
   },
+  message_area:{
+    flex: 4,
+  },
   message_box: {
     height: 85,
     flexDirection:"row",
+    margin: 8,
+    // flex:1,
   },
-  message: {
+  message_mid: {
     fontWeight:"300",
     fontSize:14,
     textAlign: "center",
     lineHeight: 40,
+    flex:2,
+    margin:8,
+  },
+  msg_name: {
+    fontWeight:"400",
+    fontSize:16,
+    textAlign: "left",
+    lineHeight: 40,
+    flex:1,
+  },
+  message: {
+    fontWeight:"300",
+    fontSize:14,
+    textAlign: "left",
+    lineHeight: 40,
+    flex:1,
+  },
+  time: {
+    fontWeight:"300",
+    fontSize:16,
+    textAlign: 'justify',
+    lineHeight: 40,
+    flex:1,
   },
 });
 
