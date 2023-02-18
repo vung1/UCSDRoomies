2d220c512cad2cbdb25c2de924fd89f7437fc68e
Danlin <jiangdl0129@gmail.com>
2023-02-17 17:43:38 -0800

    debugging for text box in chat page

--- src/components/TypeInBox/index.js
+++ src/components/TypeInBox/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import {SafeAreaView, View, StyleSheet, TextInput} from 'react-native';
+import {View, StyleSheet, TextInput} from 'react-native';
 
 const TypeInBox = () => {
   // const [text, onChangeText] = React.useState('Useless Text');
   const [number, onChangeText] = React.useState('');
-
+  const [contentHeight, setContentHeight] = React.useState({height: 90 });
+  // this.onContentSizeChange = this.onContentSizeChange.bind(this);
+  console.log("outside "+typeof(contentHeight));
   return (
     <View>
       <TextInput
         style={styles.input}
         onChangeText={onChangeText}
         placeholder="Type Something..."
-        keyboardType="default"
+        keyboardType="default" 
+        multiline={true}
+        onContentSizeChange={e=>{
+          let inputH = Math.max(e.nativeEvent.contentSize, 60)
+          if(inputH>100) inputH =100
+          console.log("inside "+typeof(inputH));
+          setContentHeight(contentHeight + inputH)
+        }}
       />
     </View>
   );
@@ -19,13 +28,16 @@ const TypeInBox = () => {
 
 const styles = StyleSheet.create({
   input: {
+    margin: 5,
     height: 60,
-    margin: 10,
-    padding: 10,
     paddingLeft:30,
+    paddingRight:30,
+    paddingTop:20,
+    paddingBotton: 20,
     borderRadius: 30,
     backgroundColor: "#F1F1F1",
     selectionColor:"#F1F1F1",
+    
   },
 });
 
