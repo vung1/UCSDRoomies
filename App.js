d238e630bc5d5d4f026e51ff9c2515336ea72dd9
Victor Ung <vung@ucsd.edu>
2023-02-22 17:23:04 -0800

    fix nav

--- App.js
+++ App.js
@@ -1,28 +1,35 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import {View, StyleSheet, SafeAreaView} from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { View, StyleSheet, SafeAreaView } from "react-native";
 
-import HomeScreen from './src/screens/HomeScreen';
-import MatchesScreen from './src/screens/MatchesScreen';
-import InitScreen from './src/screens/InitScreen';
-import ChatScreen from './src/screens/ChatScreen';
-import MainStackNavigator from './src/navigation/MainStackNavigator';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator, createAppContainer, createSwitchNavigator} from '@react-navigation/native-stack';
+import HomeScreen from "./src/screens/HomeScreen";
+import MatchesScreen from "./src/screens/MatchesScreen";
+import InitScreen from "./src/screens/InitScreen";
+import ChatScreen from "./src/screens/ChatScreen";
+import MainStackNavigator from "./src/navigation/MainStackNavigator";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createNativeStackNavigator,
+  createAppContainer,
+  createSwitchNavigator,
+} from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return (<SafeAreaView style={styles.pageContainer}>
-    <MainStackNavigator />
-    </SafeAreaView> );
+  return (
+    <SafeAreaView style={styles.pageContainer}>
+      <MainStackNavigator />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
   pageContainer: {
-    justifyContent: 'center',
+    justifyContent: "center",
     flex: 1,
-    backgroundColor:"#FFFFFF"
+    backgroundColor: "#FFFFFF",
   },
 });
 
+export default App;
--- src/screens/MatchesScreen.js
+++ src/screens/MatchesScreen.js
@@ -1,92 +1,113 @@
-import React from 'react';
-import {TouchableOpacity, View, Text, StyleSheet, SafeAreaView, Image} from 'react-native';
-import Svg, {Path} from 'react-native-svg';
+import React from "react";
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+} from "react-native";
+import Svg, { Path } from "react-native-svg";
 
+import { ScrollView } from "react-native-gesture-handler";
+import users from "../../assets/data/users";
+import ChatScreen from "./ChatScreen";
+import BackArrow from "../components/BackArrow";
 
-import { ScrollView } from 'react-native-gesture-handler';
-import users from '../../assets/data/users';
-import ChatScreen from './ChatScreen';
-import BackArrow from '../components/BackArrow';
-
-const MatchesScreen = ({navigation}) => {
+const MatchesScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.ver_container}>
       <View style={styles.container}>
-        <View style={{flexDirection: "row"}}>
-        <BackArrow navigation={navigation} screen={'MatchesScreen'} screenName={'Matches'}/>
-           <Text style={{fontWeight: 'bold', fontSize: 24, color: '#247DCF', marginLeft: 20}}>
+        <View style={{ flexDirection: "row" }}>
+          <BackArrow
+            navigation={navigation}
+            screen={"HomeScreen"}
+            screenName={"HomeScreen"}
+          />
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 24,
+              color: "#247DCF",
+              marginLeft: 20,
+            }}
+          >
             New Matches
           </Text>
         </View>
         <ScrollView style={styles.scrollView} horizontal={true}>
           <View style={styles.users}>
-            {users.map(user => (
+            {users.map((user) => (
               <View style={styles.user} key={user.id}>
-                <Image source={{uri: user.image}} style={styles.simp_image} />
+                <Image source={{ uri: user.image }} style={styles.simp_image} />
                 <Text style={styles.name}>{user.name.split(" ")[0]}</Text>
               </View>
-              
             ))}
           </View>
         </ScrollView>
       </View>
       <View style={styles.message_area}>
-      <ScrollView style={styles.scrollView} vertical={true}>
-              <View style={styles.container}>
-                {users.map(user => (
-                (user.messages) ?
-                <TouchableOpacity onPress={() =>
-                  navigation.navigate('Chat', {
-                    user: user, screen: 'Profile' 
-                  })
-                }>
+        <ScrollView style={styles.scrollView} vertical={true}>
+          <View style={styles.container}>
+            {users.map((user) =>
+              user.messages ? (
+                <TouchableOpacity
+                  onPress={() =>
+                    navigation.navigate("Chat", {
+                      user: user,
+                      screen: "Profile",
+                    })
+                  }
+                >
                   <View style={styles.message_box} key={user.id}>
-                  <View style={styles.user} key={user.id}>
-                    <Image source={{uri: user.image}} style={styles.simp_image} />
-                  </View>
-                  <View style={styles.message_mid}>
-                    <Text style={styles.msg_name}>{user.name}</Text>
-                    <Text style={styles.message}>{(user.messages.slice(-1)[0]).split("\n")[0]}</Text>
-                  </View>
-                  <View >
-                    <Text style={styles.time}>
-                    </Text>
-                    <Text style={styles.time}>{user.messages[0].split("\n").pop()}
-                    </Text>
-                  </View>
+                    <View style={styles.user} key={user.id}>
+                      <Image
+                        source={{ uri: user.image }}
+                        style={styles.simp_image}
+                      />
+                    </View>
+                    <View style={styles.message_mid}>
+                      <Text style={styles.msg_name}>{user.name}</Text>
+                      <Text style={styles.message}>
+                        {user.messages.slice(-1)[0].split("\n")[0]}
+                      </Text>
+                    </View>
+                    <View>
+                      <Text style={styles.time}></Text>
+                      <Text style={styles.time}>
+                        {user.messages[0].split("\n").pop()}
+                      </Text>
+                    </View>
                   </View>
                 </TouchableOpacity>
-                 : null
-                
-              ))}
-              </View>
-      </ScrollView>
+              ) : null,
+            )}
+          </View>
+        </ScrollView>
       </View>
-      
     </SafeAreaView>
-  
   );
 };
 
 const styles = StyleSheet.create({
   root: {
-    width: '100%',
+    width: "100%",
     flex: 1,
     padding: 10,
   },
-  ver_container:{
+  ver_container: {
     padding: 10,
     flexDirection: "column",
-    flex:1,
-    backgroundColor:"#FFFFFF"
+    flex: 1,
+    backgroundColor: "#FFFFFF",
   },
   container: {
     padding: 10,
     flex: 1,
   },
   users: {
-    flexDirection: 'row',
-    height:"30%"
+    flexDirection: "row",
+    height: "30%",
   },
   user: {
     width: 85,
@@ -96,56 +117,56 @@ const styles = StyleSheet.create({
 
     borderWidth: 2,
     padding: 3,
-    borderColor: '#247DCF',
+    borderColor: "#247DCF",
   },
- 
+
   simp_image: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     borderRadius: 50,
   },
   name: {
-    fontWeight:"300",
-    fontSize:14,
+    fontWeight: "300",
+    fontSize: 14,
     textAlign: "center",
     lineHeight: 40,
   },
-  message_area:{
+  message_area: {
     flex: 4,
   },
   message_box: {
     height: 85,
-    flexDirection:"row",
+    flexDirection: "row",
     margin: 8,
   },
   message_mid: {
-    fontWeight:"300",
-    fontSize:14,
+    fontWeight: "300",
+    fontSize: 14,
     textAlign: "center",
     lineHeight: 40,
-    flex:2,
-    margin:8,
+    flex: 2,
+    margin: 8,
   },
   msg_name: {
-    fontWeight:"400",
-    fontSize:16,
+    fontWeight: "400",
+    fontSize: 16,
     textAlign: "left",
     lineHeight: 40,
-    flex:1,
+    flex: 1,
   },
   message: {
-    fontWeight:"300",
-    fontSize:14,
+    fontWeight: "300",
+    fontSize: 14,
     textAlign: "left",
     lineHeight: 40,
-    flex:1,
+    flex: 1,
   },
   time: {
-    fontWeight:"300",
-    fontSize:16,
-    textAlign: 'justify',
+    fontWeight: "300",
+    fontSize: 16,
+    textAlign: "justify",
     lineHeight: 40,
-    flex:1,
+    flex: 1,
   },
 });
 
