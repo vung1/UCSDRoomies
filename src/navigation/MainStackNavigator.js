e2340b4c0d72f97fb140473f8e53631e009f2389
Boxiao Yan <boy033@ucsd.edu>
2023-03-08 22:07:52 -0800

    test create profile

--- src/navigation/MainStackNavigator.js
+++ src/navigation/MainStackNavigator.js
@@ -32,23 +32,16 @@ const MainStackNavigator = () => {
       >
         {user ? (
           <>
-            <Stack.Screen
-              name="CreateProfileScreen"
-              component={CreateProfileScreen}
-            ></Stack.Screen>
             <Stack.Screen name="HomeScreen" component={HomeScreen} />
             <Stack.Screen name="Matches" component={MatchesScreen} />
             <Stack.Screen name="Chat" component={ChatScreen} />
             <Stack.Screen name="Likes" component={LikesScreen} />
             <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
             <Stack.Screen name="ModelScreen" component={ModelScreen} />
+            <Stack.Screen name="CreateProfileScreen" component={CreateProfileScreen} />
           </>
         ) : (
           <>
-            <Stack.Screen
-              name="CreateProfileScreen"
-              component={CreateProfileScreen}
-            ></Stack.Screen>
             <Stack.Screen name="InitScreen" component={InitScreen} />
             <Stack.Screen name="LoginScreen" component={LoginScreen} />
             <Stack.Screen name="SigninScreen" component={SigninScreen} />
