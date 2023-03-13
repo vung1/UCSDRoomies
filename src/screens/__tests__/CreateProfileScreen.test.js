f63fea76b35950fe69bf82afc3421d8c2de723d5
brian-nguyen329 <brnguyen@ucsd.edu>
2023-03-12 23:48:06 -0700

    solved linting issues for test files
    introduced some minor issues with the test files, but will fix later

--- src/screens/__tests__/CreateProfileScreen.test.js
+++ src/screens/__tests__/CreateProfileScreen.test.js
@@ -1,20 +1,9 @@
 import React from "react";
-
-import {
-  render,
-  fireEvent,
-  screen,
-  act,
-  waitFor,
-  mount,
-} from "@testing-library/react-native";
-
 import * as renderer from "react-test-renderer";
 
 import CreateProfileScreen from "../CreateProfileScreen";
-import LoginLogo from "../../components/LoginLogo";
 
-describe('Rendering Tests', () => {
+describe("Rendering Tests", () => {
   test("matches snapshot", () => {
     const tree = renderer.create(<CreateProfileScreen />).toJSON();
     expect(tree).toMatchSnapshot();
